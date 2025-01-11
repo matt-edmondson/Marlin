dbf3a4e0122b2916b2c1e7aab97eed4d45be7b9c
Matthew Edmondson <matthew.edmondson@gmail.com>
2025-01-11 15:16:51 +1100

    Adjust nozzle clean end point to use Z_MIN_POS for improved cleaning accuracy

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -2334,7 +2334,7 @@
   // Specify positions for each tool as { { X, Y, Z }, { X, Y, Z } }
   // Dual hotend system may use { {  -20, (Y_BED_SIZE / 2), (Z_MIN_POS + 1) },  {  420, (Y_BED_SIZE / 2), (Z_MIN_POS + 1) }}
   #define NOZZLE_CLEAN_START_POINT { {  30, 30, (Z_MIN_POS + 1) } }
-  #define NOZZLE_CLEAN_END_POINT   { { 100, 60, (Z_MIN_POS + 1) } }
+  #define NOZZLE_CLEAN_END_POINT   { { 100, 60, (Z_MIN_POS) } }
 
   // Circular pattern radius
   #define NOZZLE_CLEAN_CIRCLE_RADIUS 6.5
